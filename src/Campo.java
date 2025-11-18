6a4f10e19817652ba8eacf6f901aa51feeb336c6
mattiatodesco <mattia.todesco@marconirovereto.it>
2025-11-18 21:15:28 +0100

    Feat: implementa metodi generaMine, contaIndizi, scopriCella, controllaBordi

--- src/Campo.java
+++ src/Campo.java
@@ -3,7 +3,6 @@ import java.awt.Dimension;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.Random;
-
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
@@ -84,15 +83,43 @@ public class Campo extends JPanel {
      * Posiziona le mine casualmente nelle celle
      */
     private void generaMine() {
-
         Random rand = new Random();
+
+        for (int i = 0; i < this.mine; i++) {
+            Cella newMina = campo[rand.nextInt(campo.length)][rand.nextInt(campo[0].length)];
+
+            //Se è gia una mina decremento l'indice e vado avanti, altrimenti la imposto come mina
+            if (newMina.getContenuto() == this.MINA)
+                i--;
+            else 
+                newMina.setContenuto(this.MINA);
+        }
     }
 
     /**
      * Analizza le celle adiacenti contando le mine, e imposta gli indizi numerici
      */
     private void contaIndizi() {
+        //Ciclo su tutte le celle
+        for (int r = 0; r < campo.length; r++) {
+            for (int c = 0; c < campo[0].length; c++) {
+                //Inizializzo il conto
+                int conto = 0;
+                //Prendo la cella
+                Cella cella = campo[r][c];
+                //Controllo se sta ai bordi
+                int[] bordi = controllaBordi(cella);
 
+                for (int i = bordi[0]; i <= bordi[1]; i++) {
+                    for (int j = bordi[2]; j <= bordi[3]; j++) {
+                        if (campo[i][j].getContenuto() == -1)
+                            conto++;
+                    }
+                }
+                //Inserisco il valore nella cella
+                cella.setContenuto(conto);
+            }
+        }
     }
 
     /**
@@ -101,7 +128,56 @@ public class Campo extends JPanel {
      * @param c colonna della cella di partenza
      */
     public void scopriCella(int r, int c) {
+        //Salvo la cella in una variabile
+        Cella cella = campo[r][c];
 
-        // va fatta ricorsiva!!!
+        //Controllo se è gia visibile e in caso non faccio niente
+        if (cella.isVisible()) 
+            return;
+
+        cella.setVisibile(true);
+
+        int[] bordi = controllaBordi(cella);
+        
+        //Se la cella non ha mine adiacenti scopro tutte le sue celle adiacenti
+        if (cella.getContenuto() == 0){
+            //sopra
+            scopriCella(bordi[0], bordi[2]);
+            scopriCella(bordi[0], c);
+            scopriCella(bordi[0], bordi[3]);
+            //in mezzo
+            scopriCella(r, bordi[2]);
+            scopriCella(r, bordi[3]);
+            //sotto
+            scopriCella(bordi[1], bordi[2]);
+            scopriCella(bordi[1], c);
+            scopriCella(bordi[1], bordi[3]);
+        }
+    }
+
+    /**
+     * Controlla se la cella è adiacente al bordo e ritorna un array con valori per il controllo delle celle adiacenti effettive.
+     * Per esempio se una cella sta sul bordo sinistro, le celle adiacenti effettive sono solo sopra, a destra e sotto.
+     * @param cella Cella da controllare
+     * @return Array con i valori (rMin, rMax, cMin, cMax)
+     */
+    private int[] controllaBordi(Cella cella){
+        // int rMin, rMax, cMin, cMax;
+        int[] bordi = {0,0,0,0};
+
+        //rMin
+        if (cella.getR() -1 < 0) bordi[0] = cella.getR();
+        else bordi[0] = cella.getR() -1;
+        //rMax
+        if (cella.getR() +1 >= campo.length) bordi[1] = cella.getR();
+        else bordi[1] = cella.getR() +1;
+        //cMin
+        if (cella.getC() -1 < 0) bordi[2] = cella.getC();
+        else bordi[2] = cella.getC() -1;
+        //cMax
+        if (cella.getC() +1 >= campo[0].length) bordi[3] = cella.getC();
+        else bordi[3] = cella.getC() -1;
+
+        return bordi;
     }
 }
